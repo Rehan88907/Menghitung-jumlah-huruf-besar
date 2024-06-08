2ac4757de3b98308e70f159e95db638a95d313e9
Rehan88907 <161547835+Rehan88907@users.noreply.github.com>
2024-06-08 15:27:58 +0700

    Add files via upload

---
+++ RaihanDaspro12.py
@@ -0,0 +1,9 @@
+import module
+
+text1 = "Perkenalkan , Nama Saya Raihan Daffa Abdurrahman Makanan Kesukaan saya Cibay Hobi saya Bermain FreeFire"
+text2 = "Our lives are defined by opportunities, even the ones we miss"
+text3 = "OST Official Teaser | Attack on Titan: Requiem | Studio Eclypse"
+
+print(f"Jumlah Huruf Kapital Dalam '{text1}' Adalah: {module.count_uppercase(text1)}")
+print(f"Jumlah Huruf Kapital Dalam '{text2}' Adalah: {module.count_uppercase(text2)}")
+print(f"Jumlah Huruf Kapital Dalam '{text3}' Adalah: {module.count_uppercase(text3)}")
